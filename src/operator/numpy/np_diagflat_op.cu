74ae4c92105d5390b18d05cffc5e556dd7982089
cassiniXu <cassini.Xu@outlook.com>
2019-11-14 20:04:19 +0800

    Fix FCompute error

--- src/operator/numpy/np_diagflat_op.cu
+++ src/operator/numpy/np_diagflat_op.cu
@@ -4,9 +4,9 @@ namespace mxnet{
 namespace op{
 
 NNVM_REGISTER_OP(_npi_diagflat)
-.set_attr<FCompute>("FComputr<gpu>", NumpyDiagflatOpForward<gpu>);
+.set_attr<FCompute>("FCompute<gpu>", NumpyDiagflatOpForward<gpu>);
 
-NNVM_REGISTER_OP(_backward_npi_diagflat_)
+NNVM_REGISTER_OP(_backward_npi_diagflat)
 .set_attr<FCompute>("FCompute<gpu>", NumpyDiagflatOpBackward<gpu>);
 
 } // namespace op
